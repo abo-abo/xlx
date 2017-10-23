ed7cbbb3833c43e43ed962f168b5f8398f9b1ddb
Oleh Krehel <ohwoeowho@gmail.com>
2017-10-23 21:54:11 +0200

    myscript.lisp: Take input and output as arguments
    * Cookbook.py: Demo.

--- Cookbook.py
+++ Cookbook.py
@@ -1,5 +1,17 @@
+#* Imports
+import re
+import pycook.elisp as el
+lf = el.lf
+
+#* Globals
+in_xml = "res/config.xml"
+out = "out"
+out_lisp = out + ".lisp"
+out_xml = out + ".xml"
+
+#* Recipes
 def run(recipe):
     return [
-        "rm -f ~/demo_pipeline.lisp",
-        "./myscript.lisp",
-        "cat ~/demo_pipeline.lisp"]
+        lf("rm -f {out_lisp} {out_xml}"),
+        lf("./myscript.lisp {in_xml} {out}"),
+        lf("cat {out_lisp}")]
