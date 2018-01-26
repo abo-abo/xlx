c7e5baf79f6e15b8f2a3e97389f5cb858cd662f0
Oleh Krehel <ohwoeowho@gmail.com>
2018-01-26 19:09:31 +0100

    Cookbook.py: Add a recipe to install Quicklisp

--- Cookbook.py
+++ Cookbook.py
@@ -5,3 +5,8 @@ def run(recipe):
         "./xlx.lisp res/config.xml out.lisp",
         "./xlx.lisp out.lisp out.xml",
         "diff -s res/config.xml out.xml"]
+
+def install_quicklisp(recipe):
+    return [
+        "sbcl --load res/quicklisp.lisp --eval '(progn (quicklisp-quickstart:install) (exit))'"
+    ]
